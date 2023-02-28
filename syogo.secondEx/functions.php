0a71c9a78b24ac879245fa396e34abc33447475a
satohi0000 <87967374+satohi0000@users.noreply.github.com>
2023-03-01 03:41:55 +0900

    remake

--- syogo.secondEx/functions.php
+++ syogo.secondEx/functions.php
@@ -22,9 +22,9 @@
 
     function enqueue_name(){
      
-       wp_enqueue_script('splide_name', get_template_directory_uri() . '/js/splide.js', array(), '1.0.0', true); 
+      wp_enqueue_script('splide_name', get_template_directory_uri() . '/js/splide.js', array(), '1.0.0', true); 
       wp_enqueue_style('style_name', get_template_directory_uri() . '/css/style.css', array(), '1.0.0'); 
-      wp_enqueue_script( 'jquery', 'https://code.jquery.com/jquery-3.6.0.min.js' , '', '3.6.0', true );
+      wp_enqueue_script( 'jquery', '//code.jquery.com/jquery-3.6.0.min.js' , '', '3.6.0', true );
       wp_enqueue_script('script_name', get_template_directory_uri() . '/js/script.js', array('jquery'), '1.0.0', true);
      
    }
